9abd768a1c52db04ae87fe8e108c02d5e640498f
mjshaffer117 <mjshaffer117@gmail.com>
2025-09-27 16:24:12 -0400

    function declaration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os, sys
 from prompts import system_prompt
+from functions.get_files_info import schema_get_files_info
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
@@ -18,16 +19,31 @@ def main():
     load_dotenv()
     api_key = os.environ.get('GEMINI_API_KEY')
     client = genai.Client(api_key=api_key)
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt)
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt
+        )
     )
-    print(response.text)
+
     if verbose:
         print(f'\nUser prompt: {user_prompt}')
         print(f'\nPrompt tokens: {response.usage_metadata.prompt_token_count}')
         print(f'\nResponse tokens: {response.usage_metadata.candidates_token_count}')
+    
+    function_specifications = response.function_calls
+    if function_specifications:
+        for function_spec in function_specifications:
+            print(f"Calling function: {function_spec.name}({function_spec.args})")
+    else:
+        print(response.text)
 
 
 if __name__ == "__main__":
